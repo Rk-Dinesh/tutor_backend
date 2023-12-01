649af81d273b6a9f41364f609eeb7613573cec25
Dinesh Kumar Rasu <127471925+Rk-Dinesh@users.noreply.github.com>
2023-12-01 15:24:38 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connection = mongoose.createConnection('mongodb://0.0.0.0:27017/tutor_finder').on('open',() => {
+const connection = mongoose.createConnection('mongodb://localhost:27017/tutor_finder').on('open',() => {
     console.log("MongoDB Connected");
 }).on('error',() =>{
     console.log("MongoDB Connection error");
